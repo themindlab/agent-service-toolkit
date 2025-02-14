c568b67dcbed88ead03c8e8760186dadb55ee59a
Tom Short <tom.short@themindlab.co.uk>
2025-02-14 14:30:47 +0000

    wait for query in CI

--- src/mindlab_clients/__init__.py
+++ src/mindlab_clients/__init__.py
@@ -1,9 +1,6 @@
 from core import settings
 from ml_http import auto_client
 
-print("AAAAAAAAAA")
-print(settings.HTTP_DATA_SERVER)
-print(settings.HTTP_QUERY_SERVER)
 data_client = auto_client(settings.HTTP_DATA_SERVER).http()
 query_client = auto_client(settings.HTTP_QUERY_SERVER).http()
 
