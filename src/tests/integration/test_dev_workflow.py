5823005291069b3983fbc73df6b9224edc39cbec
joe <joe@themindlab.co.uk>
2025-03-17 15:34:50 +0000

    async test passing

--- src/tests/integration/test_dev_workflow.py
+++ src/tests/integration/test_dev_workflow.py
@@ -8,7 +8,13 @@ def test_setup_data(test_client):
         })
     assert res.status_code == 200
     res_data = res.json()
-    print(res_data)
     thread_id = res_data['config']['configurable']['thread_id']
 
-    print(thread_id)
+    while(res_data['status'] == 'running'):
+        res = test_client.post("/get_thread_state",
+            json={
+                "thread_id": thread_id,
+                "workflow_id": "dev_workflow"
+            })
+        assert res.status_code == 200
+        res_data = res.json()
